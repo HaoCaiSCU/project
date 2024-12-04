8dd9cac0afd2eeca9d819c21ce074f4bb4526f60
Hao Tsai <o@Haos-MacBook-Air.local>
2024-12-04 03:17:02 -0500

    Add new changes for a6 branch

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -11,6 +11,11 @@ export const findMyCourses = async () => {
   
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
@@ -27,7 +32,39 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
-export const updateUser = async (user: any) => {
-  const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
+export const updateUser = async (user: { _id: any; }) => {
+  const response = await axios.put(`${USERS_API}/${user._id}`, user);
   return response.data;
 };
+
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+
+export function filterUsersByRole(role: string) {
+  throw new Error("Function not implemented.");
+}
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
+
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
+
+
